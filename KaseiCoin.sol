84a1dd2947a5adc1015c69945a8903bff3a889eb
billysco <70048321+billysco@users.noreply.github.com>
2021-05-23 12:14:48 -0700

    Update KaseiCoin.sol
    Added initial contract

--- KaseiCoin.sol
+++ KaseiCoin.sol
@@ -8,4 +8,12 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20Detailed.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20Mintable.sol";
 
-// Create a constructor for the KaseiCoin contract and have the contract inherit the libraries that you imported from OpenZeppelin.
+// Create the contract for KaseiCoin and have it inherit the libraries imported from OpenZeppelin
+contract KaseiCoin is ERC20, ERC20Detailed, ERC20Mintable {
+
+    // Create a constructor with parameters for name, symbol, and initial supply.
+    constructor (string memory name, string memory symbol, uint initialSupply) ERC20Detailed(name, symbol, 18) public {
+        
+    }
+    
+}
